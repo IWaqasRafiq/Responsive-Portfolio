d9cf49287e938bcd135f6fdc2bd74ae720b065cc
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-07-22 12:48:34 -0700

    initialize files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Style from  './App.module.css'
+
+function App() {
+
+  return (
+    <div className={Style.App}>
+      <h1>Welcome to My App</h1>
+    </div>
+  )
+}
+
+export default App
